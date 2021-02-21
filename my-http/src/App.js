ca4c2a72f5e53f80408f349bc69b262ad35ea9ac
sherzod <sherzodbekoffical@gmail.com>
2021-02-21 17:53:28 +0500

    ssss

---
+++ my-http/src/App.js
@@ -0,0 +1,12 @@
+import Todos from "./components/todos";
+
+function App() {
+  return (
+    <main className="container">
+      <Todos />
+    </main>
+  );
+}
+
+export default App;
+
---
+++ my-http/src/index.js
@@ -0,0 +1,13 @@
+import { render } from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+render(
+  <>
+    <ToastContainer />
+    <App />
+  </>,
+  document.getElementById("root")
+);
